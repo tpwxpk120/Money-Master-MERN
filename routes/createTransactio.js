cf2ff09019007024935a0bc0bb5e70a1f495b1e0
PengTao <129124105+tpwxpk120@users.noreply.github.com>
2023-12-11 15:35:37 -0800

    Delete routes directory

--- routes/createTransactio.js
+++
@@ -1,9 +0,0 @@
-import { connectToMongoDB } from "../db/mydb.js";
-
-
-const createTransactionSchema = async (req, res) => {
-  const db = await connectToMongoDB("test");
-  return db.collection("");
-};
-
-export { createTransactionSchema };
